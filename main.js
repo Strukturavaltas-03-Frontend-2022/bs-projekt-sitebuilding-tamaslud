a9295668d99ebc01b83679cf53c917845feaffd8
tamaslud <tamaslud@yahoo.com>
2022-10-08 23:00:18 +0200

    buttons with yellow background

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ addEventListener('scroll', () => {
     if (displayScrolled !== Boolean(window.scrollY)){
         displayScrolled = Boolean(window.scrollY);
         if(displayScrolled) {
-            navbar.classList.add('bg-secondary');
+            navbar.classList.add('bg-white');
             navbar.classList.remove('bg-transparent');
         } else{
             navbar.classList.add('bg-transparent');
-            navbar.classList.remove('bg-secondary');
+            navbar.classList.remove('bg-white');
         };
     };
 });
