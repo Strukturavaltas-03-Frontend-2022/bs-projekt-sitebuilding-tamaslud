7e05183098054863a8945e11b1f5b9edcf21d445
tamaslud <tamaslud@yahoo.com>
2022-10-08 21:27:59 +0200

    fixed navbar

---
+++ main.js
@@ -0,0 +1,14 @@
+let displayScrolled = false;
+const navbar = document.querySelector('.navbar');
+addEventListener('scroll', () => {
+    if (displayScrolled !== Boolean(window.scrollY)){
+        displayScrolled = Boolean(window.scrollY);
+        if(displayScrolled) {
+            navbar.classList.add('bg-secondary');
+            navbar.classList.remove('bg-transparent');
+        } else{
+            navbar.classList.add('bg-transparent');
+            navbar.classList.remove('bg-secondary');
+        };
+    };
+});
